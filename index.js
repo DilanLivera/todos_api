6bc2dbbcce0620d81d7708dcb9e32ed5f36fafec
Dilan Livera <dylan_yahampath@yahoo.com>
2019-04-08 00:02:49 +0000

    add index page and styling to index page

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const express = require("express"),
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); //above line and this line helps us to access data sent in a post or put request
 app.use('/api/todos', toDoRoutes);
-app.set("view engine", "ejs");
+// app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views')); //__dirname is current directory name
     
-app.get('/', (req, res) => res.render("index"));
+app.get('/', (req, res) => res.sendFile("index.html"));
     
 app.listen(port, () => console.log(`Server is runnin on port ${port}`));
